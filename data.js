c6e8dace26ba5a705f685c59be6f4ac2119f4dc0
DskterSahil <dsktersahil@gmail.com>
2024-07-15 21:07:49 +0530

    Commit to initial files

---
+++ data.js
@@ -0,0 +1,57 @@
+export const tweetsData = [   
+    {
+        handle: `@RvcjMedia123 `,
+        profilePic: `images/rvcjlogo.jpg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `My 2 digit bank balance secured with 4 digit UPI pin 😢😢.`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@ElonMusk ✅`,
+        profilePic: `images/musk.png`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `You’re wasting my time. I literally rule the world 🌍.`,
+        replies: [
+                  {
+                handle: `@TomCruise ✅`,
+                profilePic: `images/tcruise.png`,
+                tweetText: `Okayyy! 😎😂😂`,
+            },
+                  {
+                handle: `@AmericanPyscho ✅`,   
+                profilePic: `images/christian-bale.jpg`,
+                tweetText: `How well does it pay? 😎😎`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@NoobCoder12`,
+        profilePic: `images/flower.png`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `Are you a coder if you only know HTML?`,
+        replies: [
+            {
+                handle: `@StackOverflower ☣️`,
+                profilePic: `images/overflow.png`,
+                tweetText: `No. Obviosuly not. Go get yourself auto-rickshaw.`,
+            },
+            {
+                handle: `@YummyCoder64`,
+                profilePic: `images/love.png`,
+                tweetText: `You need some help!! 😑😑.`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
